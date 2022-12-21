399956a6e303e5f1252a562dc9ecf8ef053c1236
einhornmarie <42034238+einhornmarie@users.noreply.github.com>
2022-12-21 21:36:23 +0100

    [#2] Add test for BoolExpression (#8)

---
+++ types/bool_expression_test.go
@@ -0,0 +1,98 @@
+package types
+
+import (
+	. "imp/helper"
+	"testing"
+)
+
+// TestBool tests the Bool function
+
+type TestBoolCase struct {
+	input     bool
+	want      BoolExpression
+	compliant bool
+}
+
+var testBoolTests = []TestBoolCase{
+	{true, BoolExpression(true), true},
+	{false, BoolExpression(false), true},
+	{true, BoolExpression(false), false},
+	{false, BoolExpression(true), false},
+}
+
+func TestBool(t *testing.T) {
+	for _, test := range testBoolTests {
+		if got := Bool(test.input); got != test.want && test.compliant {
+			t.Errorf("got %q not equal to want %q", got, test.want.Pretty())
+		}
+	}
+}
+
+// TestPretty tests the Pretty function
+
+type TestBoolPrettyCase struct {
+	input     BoolExpression
+	want      string
+	compliant bool
+}
+
+var testBoolPrettyTests = []TestBoolPrettyCase{
+	{BoolExpression(true), "true", true},
+	{BoolExpression(false), "false", true},
+	{BoolExpression(true), "false", false},
+	{BoolExpression(false), "true", false},
+}
+
+func TestBoolPretty(t *testing.T) {
+	for _, test := range testBoolPrettyTests {
+		if got := test.input.Pretty(); got != test.want && test.compliant {
+			t.Errorf("got %q not equal to want %q", got, test.want)
+		}
+	}
+}
+
+// TestEval tests the Eval function
+
+type TestBoolEvalCase struct {
+	input     BoolExpression
+	want      Value
+	compliant bool
+}
+
+var testBoolEvalTests = []TestBoolEvalCase{
+	{BoolExpression(true), BoolValue(true), true},
+	{BoolExpression(false), BoolValue(false), true},
+	{BoolExpression(true), BoolValue(false), false},
+	{BoolExpression(false), BoolValue(true), false},
+}
+
+func TestBoolEval(t *testing.T) {
+	for _, test := range testBoolEvalTests {
+		if got := test.input.Eval(ValueState{}); got != test.want && test.compliant {
+			t.Errorf("got %q not equal to want %q", StructToJson(got), StructToJson(test.want))
+		}
+	}
+}
+
+type TestBoolInferCase struct {
+	input     BoolExpression
+	want      Type
+	compliant bool
+}
+
+var testBoolInferTests = []TestBoolInferCase{
+	{BoolExpression(false), TypeBool, true},
+	{BoolExpression(true), TypeBool, true},
+	{BoolExpression(true), TypeInt, false},
+	{BoolExpression(false), TypeInt, false},
+}
+
+// TestInfer tests the Infer function
+
+func TestBoolInfer(t *testing.T) {
+	for _, test := range testBoolInferTests {
+		if got := test.input.Infer(TypeState{}); got != test.want && test.compliant {
+			t.Errorf("got %q not equal to want %q", got, test.want)
+		}
+	}
+}
